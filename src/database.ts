3a1a04759cebe92a4e03340f6bb40edd1d66bd3f
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-12-18 21:43:18 -0300

    fixed entities path

--- src/database.ts
+++ src/database.ts
@@ -8,13 +8,15 @@ if (
 }
 
 export default async function connect() {
-  const connectionManager = await getConnectionManager();
+  const connectionManager = getConnectionManager();
   const connection = connectionManager.create({
     name: 'default',
     type: 'postgres',
     url: process.env.DATABASE_URL,
     entities: [
-      `${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`,
+      `${
+        process.env.NODE_ENV === 'production' ? 'dist/src' : 'src'
+      }/entities/*.*`,
     ],
     ssl: process.env.NODE_ENV === 'production',
   });
