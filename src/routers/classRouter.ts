d49b04bdd6153ac0cf2c11f6fd72a56f4a5502b1
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-12-20 14:02:36 -0300

    removed commented code

--- src/routers/classRouter.ts
+++ src/routers/classRouter.ts
@@ -3,12 +3,6 @@ import * as classController from '../controllers/classController';
 
 const router = express.Router();
 
-// router.get('/', async (req: Request, res: Response) => {
-//   const result = await getRepository(ClassToProfessor).find();
-
-//   return res.send(result).status(200);
-// });
-
 router.get('/', classController.getClasses);
 router.get('/:id', classController.getExamsFromClass);
 
