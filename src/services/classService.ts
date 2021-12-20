8f5533a64ac5c9111956a8ddbe0fe5a40e772d8b
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-12-19 22:39:44 -0300

    finished upload exam endpoint

--- src/services/classService.ts
+++ src/services/classService.ts
@@ -2,8 +2,8 @@ import { getRepository } from 'typeorm';
 import Class from '../entities/Class';
 
 export async function getClasses() {
-  const classes = (await getRepository(Class).find()).map((_class) =>
-    _class.getClasses()
+  const classes = Promise.all(
+    (await getRepository(Class).find()).map((_class) => _class.getClasses())
   );
   return classes;
 }
