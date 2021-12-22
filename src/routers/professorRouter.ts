eb69b4d7b951df99c82634e34a605b371ca1b883
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-12-22 14:35:10 -0300

    prepara revisão

--- src/routers/professorRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as professorController from '../controllers/professorController';
-
-const router = express.Router();
-
-router.get('/', professorController.getProfessors);
-router.get('/:id', professorController.getExamsFromProfessor);
-
-export default router;
--- src/services/classService.ts
+++
@@ -1,9 +0,0 @@
-import { getRepository } from 'typeorm';
-import Class from '../entities/Class';
-
-export async function getClasses() {
-  const classes = Promise.all(
-    (await getRepository(Class).find()).map((_class) => _class.getClasses())
-  );
-  return classes;
-}
